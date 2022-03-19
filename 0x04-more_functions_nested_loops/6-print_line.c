9629b38783d62a4263a54b89b256f18de48c5e7b
root <essenowoidee@gmail.com>
2022-03-18 18:23:32 -0700

    added to task 8

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "mian.h"
+/**
+ * print_linr - print a line
+ * @n: numbers of straight lines
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int l;
+
+	for (l = 1; l <= n; l++)
+	{
+		_putchar(95);
+	}
+
+	_putchar('\n');
+}
+

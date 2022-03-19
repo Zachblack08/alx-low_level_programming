15c3ef2590276697648564791395bd3ca19139c4
root <essenowoidee@gmail.com>
2022-03-18 19:28:01 -0700

    added to task 101

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,23 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print 0 to 9 jump 2 and 4
- *
- *
+ * print_most_numbers - Entry point
+ * Description: A function that prints the numbers,
+ * from 0 to 9, followed by a new line.
+ * Does not print 2 and 4
+ * @void: it receives no parameter ..
+ * Return: Returns nothing
  */
+
 void print_most_numbers(void)
 {
-	char i;
-	for (i = '0'; i <= '9'; i++)
-	{                                                                 		if (i != '2' && i != '4')
-	_putchar(i);
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+	if (i != 2 && i != 4)
+	{
+		_putchar(i + '0');
 	}
-	_putchar('\n');
+	}
+	        _putchar('\n');
 }

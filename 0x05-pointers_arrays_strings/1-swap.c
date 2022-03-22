23368fb892b42dae4cf349b841be310b23e2543e
root <essenowoidee@gmail.com>
2022-03-22 14:33:10 -0700

    added task 1 and task 2

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * * swap_int - swaps the values
- * *@a: pointer to a
- * *@b: ponter to b
- * *Return: Nothing
- * */
+ * swap_int - swaps the values
+ *@a: pointer to a
+ *@b: ponter to b
+ *Return: Nothing
+ */
 void swap_int(int *a, int *b)
 {
 		int aux;

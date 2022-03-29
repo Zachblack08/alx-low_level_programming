da6eb02c0f365393f2cdf9b01477dfd49dbfb1b7
root <essenowoidee@gmail.com>
2022-03-28 18:27:05 -0700

    added to task 5

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,17 +1,15 @@
-#include "main.h"
-/**
- *_memset - fills memory with a constant byte,
- *@s: memory area.
- *@b: constant byte.
- *@n: bytes filled.
- *Return: the pointer to dest.
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-	for (i = 0; i < n; i++)
-		*(s + i) =  b;
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+int _putchar(char);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
-	return (s);
-}
+#endif

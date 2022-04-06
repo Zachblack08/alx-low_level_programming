33a5e1f50579c9ef52c2952a6f8b3362bac57a61
root <essenowoidee@gmail.com>
2022-04-06 10:09:28 -0700

    added task

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,8 +10,7 @@
 int count_word(char *s)
 {
 	int flag, c, w;
-	flag = 0;
-
+	flag = 0;   
 	w = 0;
 	for (c = 0; s[c] != '\0'; c++)
 	{
@@ -36,8 +35,7 @@ char **strtow(char *str)
 {
 	char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
-	while (*(str + len))
-
+	while (*(str + len)) 
 		len++;
 	words = count_word(str);
 	if (words == 0)

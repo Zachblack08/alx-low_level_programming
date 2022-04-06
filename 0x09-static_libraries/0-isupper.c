14c1f8d547f081ea1cfd0d7150121b12104f2a95
root <essenowoidee@gmail.com>
2022-04-06 05:15:24 -0700

    added task

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- *  * _isupper - check for upper case letter
- *   * @c : character to check the case
- *    * Return:0 or 1
- *     */
+ * _isupper - check for upper case letter
+ * @c : character to check the case
+ * Return:0 or 1
+ */
 
 int _isupper(int c)
 {
